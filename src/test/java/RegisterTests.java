d29c58604a0d3dae10f03ceb48c9fcefdc2d7128
YanaTsiFi <yanagrischechko@gmail.com>
2025-04-21 14:06:31 +0300

    Added Tests

---
+++ src/test/java/RegisterTests.java
@@ -0,0 +1,142 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.is;
+
+public class RegisterTests {
+
+    /*
+        1. Make request (POST) to https://reqres.in/api/register
+           with body { "email": "eve.holt@reqres.in", "password": "pistol" }
+        2. Get response { "id": 4, "token": "QpwL5tke4Pnpja7X4" }
+        3. Check "id" is 4, "token" is "QpwL5tke4Pnpja7X4", and status code 200
+     */
+    @Test
+    void successfulRegisterTest() {
+        String registerData = "{\"email\": \"eve.holt@reqres.in\", \"password\": \"pistol\"}";
+
+        given()
+                .body(registerData)
+                .contentType(JSON)
+                .log().uri()
+
+                .when()
+                .post("https://reqres.in/api/register")
+
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("id", is(4))
+                .body("token", is("QpwL5tke4Pnpja7X4"));
+    }
+
+    @Test
+    void unsuccessfulRegister400Test() {
+        String registerData = "";
+
+        given()
+                .body(registerData)
+                .contentType(JSON)
+                .log().uri()
+
+                .when()
+                .post("https://reqres.in/api/register")
+
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("error", is("Missing email or username"));
+    }
+
+    @Test
+    void userNotFoundRegisterTest() {
+        String registerData = "{\"email\": \"not.exist@reqres.in\", \"password\": \"pistol\"}";
+
+        given()
+                .body(registerData)
+                .contentType(JSON)
+                .log().uri()
+
+                .when()
+                .post("https://reqres.in/api/register")
+
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("error", is("Note: Only defined users succeed registration"));
+    }
+
+    @Test
+    void missingPasswordRegisterTest() {
+        String registerData = "{\"email\": \"eve.holt@reqres.in\"}";
+
+        given()
+                .body(registerData)
+                .contentType(JSON)
+                .log().uri()
+
+                .when()
+                .post("https://reqres.in/api/register")
+
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("error", is("Missing password"));
+    }
+
+    @Test
+    void missingEmailRegisterTest() {
+        String registerData = "{\"password\": \"pistol\"}";
+
+        given()
+                .body(registerData)
+                .contentType(JSON)
+                .log().uri()
+
+                .when()
+                .post("https://reqres.in/api/register")
+
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("error", is("Missing email or username"));
+    }
+
+    @Test
+    void wrongBodyRegisterTest() {
+        String registerData = "{%}";
+
+        given()
+                .body(registerData)
+                .contentType(JSON)
+                .log().uri()
+
+                .when()
+                .post("https://reqres.in/api/register")
+
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400);
+    }
+
+    @Test
+    void unsupportedMediaTypeRegisterTest() {
+        given()
+                .log().uri()
+
+                .when()
+                .post("https://reqres.in/api/register")
+
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(415);
+    }
+}
